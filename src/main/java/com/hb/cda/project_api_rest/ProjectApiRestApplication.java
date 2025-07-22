a6f204a4c33757fbebe40b10d25ce1a854100829
Nahima697 <nahima.toumi697@gmail.com>
2025-07-22 14:46:26 +0200

    first commit

---
+++ src/main/java/com/hb/cda/project_api_rest/ProjectApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.hb.cda.project_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hb/cda/project_api_rest/ProjectApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb.cda.project_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
